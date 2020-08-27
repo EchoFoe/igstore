75602fd6c6ce6c92075bb25fb30184a143f1b68d
Echo <aspirpd@gmail.com>
2020-08-27 10:24:50 +0300

    #22 - again prepopulated =(

--- igstore/urls.py
+++ igstore/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),
+    # path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('catalog/', include('products.urls', namespace='products')),
     path('repair/', include('repairs.urls', namespace='repairs')),
